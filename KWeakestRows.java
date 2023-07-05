7c4236486d0fa7ae1c7c71ae1737388c1d800dc6
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-07-05 15:59:24 +0530

    Add files via upload

---
+++ KWeakestRows.java
@@ -0,0 +1,46 @@
+package Arrays2D;
+
+import java.util.Arrays;
+
+public class KWeakestRows {
+
+    static int[] kWeakest(int [][]mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int pairs[][] = new int[m][2];
+        for(int i = 0; i < m; i++) {
+            int strength = 0;
+            for(int j = 0; j < n; j++) {
+                if(mat[i][j] == 0) {
+                    break;
+                }
+                strength++;
+            }
+            pairs[i][0] = strength;
+            pairs[i][1] = i;
+        }
+        Arrays.sort(pairs, (a,b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
+            else return a[0] - b[0];
+        });
+
+        int []indexes = new int[k];
+        for(int i = 0; i < k; i++) {
+            indexes[i] = pairs[i][1];
+        }
+        return indexes;
+    }
+
+    public static void main(String[] args) {
+        int mat[][] = {{1,1,0,0,0},
+                        {1,1,1,1,0},
+                        {1,0,0,0,0},
+                        {1,1,0,0,0},
+                        {1,1,1,1,1}};
+        int k = 3;
+        int output[] = kWeakest(mat, k);
+        for(int i = 0; i < output.length; i++) {
+            System.out.println(output[i]);
+        }
+    }
+}
---
+++ PascalTriangle.java
@@ -0,0 +1,35 @@
+package Arrays2D;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+
+    static List<List<Integer>> pascal(int rows) {
+        List<List<Integer>> tri = new ArrayList<>();
+        tri.add(new ArrayList<>());
+        tri.get(0).add(1);
+
+        for(int i = 1; i < rows; i++) {
+            List<Integer> row = new ArrayList<>();  // current row
+            List<Integer> prevRow = tri.get(i - 1); // previous row
+            // first element of row is 1
+            row.add(1);
+
+            for(int j = 1; j < i; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            // last element of row is 1
+            row.add(1);
+            tri.add(row);
+        }
+
+        return tri;
+    }
+    public static void main(String[] args) {
+        int rows = 5;
+        List<List<Integer>> ans = pascal(rows);
+        System.out.println(ans);
+    }
+}
