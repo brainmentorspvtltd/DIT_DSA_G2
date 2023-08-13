c752ea97bfa2d2b46d4ed1577195b30c945ba7b6
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-08-13 17:29:37 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Example {
+    public static void main(String[] args) {
+        // List<Integer> list = Arrays.asList(10,20,100,50,90);
+        // list.add(100);
+        // list.remove(0);
+        // list.set(0, 10000);
+        // System.out.println(list);
+        // Collections.sort(list);
+        // System.out.println(list);
+        //Set<String> songs =new HashSet<>();
+        // Set<String> songs = new TreeSet<>();
+        // songs.add("Pump it up");
+        // songs.add("It's My Life");
+        // songs.add("Bang Bang");
+        // songs.add("Bang Bang");
+        // songs.add("Bang Bang");
+        // System.out.println(songs);
+
+        // Map<String, Integer> phoneMap = new TreeMap<>();
+        // phoneMap.put("amit", 2222);
+        // phoneMap.put("ram", 4444);
+        // phoneMap.put("shyam", 6666);
+        // phoneMap.put("amit", 3333);
+        // phoneMap.put("tim", 6666);
+        // System.out.println(phoneMap);
+        Map<String, Map<String, List<Integer>>> map = new HashMap<>();
+        // Map<String, List<Integer>> phoneMap = new TreeMap<>();
+        // List<Integer> l1 = Arrays.asList(2222,3333);
+        // phoneMap.put("amit", l1);
+        // l1 = Arrays.asList(5555,3333);
+        // phoneMap.put("ram", l1);
+        // System.out.println(phoneMap);
+
+        //System.out.println(list.toString());
+        //List<Integer> list = new ArrayList<>();
+        //list = new Vector<>();
+        //list = new LinkedList<>();
+        // list.add(10);
+        // list.add(20);
+        // list.add(100);
+        // list.add(50);
+        // list.add(90);
+        
+
+    }
+}
